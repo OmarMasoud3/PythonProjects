ede9f15b87809098bcf9f65ae09be3655704831b
Omar Masoud <omarjaffar3@gmail.com>
2023-05-13 08:28:45 +0300

    added finished projects

---
+++ ISS_Overhead_Notifier/main.py
@@ -0,0 +1,65 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+import os
+from twilio.rest import Client
+
+MY_EMAIL = os.environ['MY_EMAIL']
+MY_PASSWORD = os.environ['MY_PASSWORD']
+MY_LAT = int(os.environ['MY_LAT'])
+MY_LONG = int(os.environ['MY_LONG'])
+ACC_SID = os.environ["ACC_SID"]
+AUTH_TOKEN = os.environ["AUTH_TOKEN"]
+TWILIO_PNO = os.environ["TWILIO_PNO"]
+MY_PNO = os.environ["MY_PNO"]
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the iss position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you."
+        )
+        client = Client(ACC_SID, AUTH_TOKEN)
+        message = client.messages.create(
+            body="Look Up, The ISS is above you.",
+            from_=TWILIO_PNO,
+            to=MY_PNO
+        )
